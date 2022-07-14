9156cc6a910485a28adf142532902fd36d51d692
Richard Blechinger <hello@pretzelhands.com>
2022-07-14 16:06:27 +0200

    Seed 10 random users

---
+++ server/seeds/users.js
@@ -0,0 +1,16 @@
+const { faker } = require('@faker-js/faker')
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  const people = [... new Array(10).keys()].map((index) => ({
+    id: index + 1,
+    name: faker.name.findName(),
+    avatar: faker.image.avatar({ width: 128, height: 128 }),
+  }))
+
+  await knex('users').del()
+  await knex('users').insert(people)
+}

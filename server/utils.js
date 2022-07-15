13535c87a92f118074ece5abc94525d28341c305
Richard Blechinger <hello@pretzelhands.com>
2022-07-15 15:32:41 +0200

    Fix relative timestamps not showing correct time

--- server/utils.js
+++ server/utils.js
@@ -5,7 +5,10 @@ TimeAgo.addDefaultLocale(en)
 const timeAgo = new TimeAgo('en-US')
 
 const dateToRelativeTime = (dateString) => {
-    const date = new Date(dateString)
+    // Z (Zulu) ensures that JavaScript is aware of this being a UTC time.
+    // SQLite datetimes come without a timezone marker, which is
+    // interpreted by JS as this being server local time, which may differ.
+    const date = new Date(`${dateString}Z`)
     return timeAgo.format(date)
 }
 

96c4fcd67d28e683d7d1c82fcc5b7ba336d2d353
Richard Blechinger <hello@pretzelhands.com>
2022-07-15 11:16:16 +0200

    Add comments module, implement route to display all comments

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
+
 app.use(express.json())
 app.use(cors())
+app.use('/api/comments', require('./comments'))
 
 app.listen(3000, () => console.log('App started'))
---
+++ server/comments.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('./db')
+// const {Comment} = require('./models')
+
+// GET /api/comments
+router.get('/', async (req, res) => {
+    const rawComments = await db.knex.raw(`
+        SELECT
+            c.*,
+            u.*,
+            count(DISTINCT up.id) AS upvotes
+        FROM comments c
+        JOIN upvotes up ON c.id = up.comment_id
+        JOIN users u ON c.user_id = u.id
+        GROUP BY c.id
+    `)
+
+    const comments = rawComments.map(data => ({
+        id: data.id,
+        text: data.text,
+        createdAt: data.created_at,
+        upvotes: data.upvotes,
+        user: {
+            id: data.user_id,
+            avatar: data.avatar,
+            name: data.name,
+        }
+    }));
+
+    res.json(comments)
+})
+
+// POST /api/comments
+router.post('/', (req, res) => {
+
+})
+
+// POST /api/comments/:commentId/upvote
+router.post('/:commentId/upvote', (req, res) => {
+    const {commentId} = req.params
+})
+
+module.exports = router
